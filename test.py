4181e95a47f144a406a53b405fac393306715633
ykawabata313 <yuya313.72@gmail.com>
2020-08-27 13:15:33 +0900

    change

--- test.py
+++ test.py
@@ -5,22 +5,6 @@ import numpy as np
 
 camera = cv2.VideoCapture(0)
 
-def red_detect(img):
-    # HSV色空間に変換
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # 赤色のHSVの値域1
-    hsv_min = np.array([0,127,0])
-    hsv_max = np.array([30,255,255])
-    mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
-
-    # 赤色のHSVの値域2
-    hsv_min = np.array([150,127,0])
-    hsv_max = np.array([179,255,255])
-    mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
-    
-    return mask1 + mask2
-
 #clickイベント
 def btn_click():
 
@@ -84,34 +68,6 @@ def btn_click():
 
     #ウィンドウを削除
     root.destroy()
-
-def main():
-    videofile_path = "video1.mp4"
-
-    # カメラのキャプチャ
-    cap = cv2.VideoCapture(videofile_path)
-    
-    while(cap.isOpened()):
-        # フレームを取得
-        ret, frame = cap.read()
-
-        # 赤色検出
-        mask = red_detect(frame)
-
-        # 結果表示
-        cv2.imshow("Frame", frame)
-        cv2.imshow("Mask", mask)
-
-        # qキーが押されたら途中終了
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__=='__main__':
-    btn_click()
-    main()
 
 #画面の設定
 root = tkinter.Tk()             # Tkクラス生成
