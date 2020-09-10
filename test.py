a638f18eafc1dc9f5e2d7323c18417529e894e0a
ykawabata313 <yuya313.72@gmail.com>
2020-09-10 15:01:27 +0900

    Completed

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import cv2
 import time
 import tkinter
 import numpy as np
+<<<<<<< HEAD
+=======
+from moviepy.editor import *
+>>>>>>> test
 
 camera = cv2.VideoCapture(0)
 
@@ -9,26 +13,31 @@ camera = cv2.VideoCapture(0)
 def btn_click():
 
     #入力された数値をcountに代入
-    count = txt.get()
-    #動画の秒数を計算(30FPS ×　入力値　＝　描画枚数)
-    movie_second = 30 * int(count)  
+    global count_mae
+    count_mae = int(txt1.get())
+    global count_ato
+    count_ato = int(txt2.get())
+    #動画の秒数を計算(29FPS ×　入力値　＝　描画枚数)
+    movie_second_mae = 29 * count_mae  
+    movie_second_ato = 29 * count_ato
 
     buffer = []
 
-#-----------------------------------------------------------
-# 動画ファイル保存用の設定
+    #-----------------------------------------------------------
+    # 動画ファイル保存用の設定
     fps = int(camera.get(cv2.CAP_PROP_FPS))                         # カメラのFPSを取得
     w = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))                   # カメラの横幅を取得
     h = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))                  # カメラの縦幅を取得
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')             # 動画保存時のfourcc設定（mp4用）
     video1 = cv2.VideoWriter('video1.mp4', fourcc, 15, (w, h))      # 動画の仕様（ファイル名、fourcc, FPS, サイズ）
     video2 = cv2.VideoWriter('video2.mp4', fourcc, fps, (w, h))     # 動画の仕様（ファイル名、fourcc, FPS, サイズ）
-#-----------------------------------------------------------
+    #-----------------------------------------------------------
 
     
-#現在撮影中の動画表示プログラム
+    #現在撮影中の動画表示プログラム
     while True:
         ret, frame1 = camera.read()
+<<<<<<< HEAD
 
         # フレームをHSVに変換
         hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
@@ -44,41 +53,47 @@ def btn_click():
         cv2.imshow("1", frame1)
         cv2.imshow("Color Image", img_color)
 
+=======
+>>>>>>> test
         buffer.append(frame1)
+        cv2.imshow("1", frame1)    
         if cv2.waitKey(1) & 0xFF == ord('q'): 
             break
-    
 
-#数秒前の描写を保存しておくためのプログラム
-    for i in range(len(buffer) - movie_second ,len(buffer)):
+
+    #数秒前の描写を保存しておくためのプログラム
+    for i in range(len(buffer) - movie_second_mae ,len(buffer)):
         frame2 = buffer.pop(i)    
-        cv2.imshow("0", frame2)                                
+        cv2.imshow("0", frame2)                
         buffer.append(frame1)                                     
-        video1.write(frame2)                                   #動画１を保存
+        video1.write(frame2)                                   #動画１を保存 
 
     start_time = time.time()
 
-#動画２の撮影と保存用プログラム
+        #動画２の撮影と保存用プログラム
     while True:
         ret, frame = camera.read()                             # フレームを取得
-        video2.write(frame)                                    # 動画を1フレームずつ保存する
+        video2.write(frame)                                    # 動画2を1フレームずつ保存する
         cv2.imshow('2', frame)                                 # フレームを画面に表示
 
         finish_time = time.time()
         passed_time = finish_time - start_time
 
-        if int(passed_time) == int(count):
+        if int(passed_time) == int(count_ato):
             break
         if cv2.waitKey(1) & 0xFF == ord('q'): 
             break
     
 
-#カメラを開放
+    #カメラを開放
     camera.release()
     cv2.destroyAllWindows()
 
     #ウィンドウを削除
     root.destroy()
+
+#-------------------------------------------------
+#UIの設定です。
 
 #画面の設定
 root = tkinter.Tk()             # Tkクラス生成
@@ -87,16 +102,32 @@ root.title('テキストボックス')     #画面タイトル
 root.configure(bg='red')
 
 #ラベル
-lbl = tkinter.Label(text='time setting...')
+lbl = tkinter.Label(text='movie time setting...')
 lbl.place(x=30,y=70)
+lbl = tkinter.Label(text='何秒前の動画が欲しい？')
+lbl.place(x=30,y=100)
+lbl = tkinter.Label(text='何秒後の動画が欲しい？')
+lbl.place(x=30,y=130)
 
 #テキストボックス
-txt = tkinter.Entry(width=20)
-txt.place(x=90,y=100)
+txt1 = tkinter.Entry(width=5)
+txt1.place(x=180,y=100)
+txt2 = tkinter.Entry(width=5)
+txt2.place(x=180,y=130)
 
 # ボタン
 btn = tkinter.Button(root, text='start!', command=btn_click)
-btn.place(x=130, y=140)
+btn.place(x=130, y=160)
 
 # 表示
 root.mainloop()
+#-------------------------------------------------
+
+
+# Vieo1の動画時間をカットして修正
+file_path = 'video1.mp4'
+start = 0
+end = count_mae
+save_path = 'video1_re.mp4'
+video = VideoFileClip(file_path).subclip(start, end)
+video.write_videofile(save_path, fps=29)
