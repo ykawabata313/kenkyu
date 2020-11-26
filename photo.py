c9941c7c2f3a92b438f0be13e83660699d2dae9c
ykawabata313 <yuya313.72@gmail.com>
2020-11-26 13:48:57 +0900

    add sample

--- photo.py
+++ photo.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+i = 100
+while i < 200:
     # フレームをキャプチャする
     ret, frame = cap.read()
 
     # 画面に表示する
     cv2.imshow('frame',frame)
-    
-    for i in range(1,100):
-        # キーボード入力待ち
-        key = cv2.waitKey(1) & 0xFF
-        # sが押された場合は保存する
-        if key == ord('s'):
-            path = "image/origin/true/t_{}.jpg".format(i)
-            cv2.imwrite(path,frame)
-        # qが押された場合は終了する
-        elif key == ord('q'):
-            break
+    path = "image/origin/false/f_{}.jpg".format(i)
+    cv2.imwrite(path,frame)
+    time.sleep(0.5)
+    i+=1
+    # キーボード入力待ち
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
 
 # キャプチャの後始末と，ウィンドウをすべて消す
 cap.release()
